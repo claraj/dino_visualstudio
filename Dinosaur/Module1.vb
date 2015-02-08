fe3c7b02605374367b91498b65fed3bc2071aefc
clara <clara.james@minneapolis.edu>
2015-02-08 10:24:23 -0600

    Created Dinosaur class with Roar method

--- Dinosaur/Module1.vb
+++ Dinosaur/Module1.vb
@@ -4,6 +4,17 @@
 
         Console.WriteLine("Dinosaur Program")
         Console.WriteLine("What is the name of your dinosaur?")
+
     End Sub
 
 End Module
+
+
+Class dinosaur
+
+    Dim name As String
+    Sub roar()
+        Console.WriteLine("grrrrrrr!!!!")
+    End Sub
+
+End Class
